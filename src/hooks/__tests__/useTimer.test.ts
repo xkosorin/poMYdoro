68bf33fad51b200673f6155e02103146c7e5e5ef
Kristian Kosorin <kristian@kosorin.eu>
2023-03-06 20:46:11 +0100

    Added tests for Timer hook

--- src/hooks/__tests__/useTimer.test.ts
+++ src/hooks/__tests__/useTimer.test.ts
@@ -1,15 +1,24 @@
-import { renderHook } from "@testing-library/react-hooks";
+import { act, renderHook } from "@testing-library/react-hooks";
 import useTimer from "../useTimer";
 
 describe("Tests of useTimer custom hook", () => {
   const TIME_INTERVAL = "sec";
+  const TIME_AMOUNT = 5;
 
-  it("should be correctly initialized.", () => {
+  it("should be correctly initialized without time.", () => {
     const { result } = renderHook(() => useTimer(TIME_INTERVAL));
-
     const [count, isRunning, isFinished] = result.current;
 
     expect(count).toBe(0);
+    expect(isRunning).toBe(false);
+    expect(isFinished).toBe(false);
+  });
+
+  it("should be correctly initialized witho time.", () => {
+    const { result } = renderHook(() => useTimer(TIME_INTERVAL, TIME_AMOUNT));
+    const [count, isRunning, isFinished] = result.current;
+
+    expect(count).toBe(5);
     expect(isRunning).toBe(false);
     expect(isFinished).toBe(false);
   });
